f20155715716c1e1010f949391ba336f9c7fd3e6
Nikita S <nikita.subbotin@itomy.ch>
2019-04-18 10:33:18 +0300

    Added Flatlist component to the feed screen

--- src/screens/feed.js
+++ src/screens/feed.js
@@ -1,38 +1,61 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, FlatList } from "react-native";
 
 class FeedScreen extends Component {
+  state = {
+    photo_feed: [0, 1, 2, 3, 4],
+    refresh: false
+  };
+
+  loadNew = () => {
+    this.setState({
+      refresh: true
+    });
+    this.setState({
+      photo_feed: [5, 6, 7, 8, 9],
+      refresh: false
+    });
+  };
+
   render() {
+    const { refresh, photo_feed } = this.state;
     return (
       <View style={styles.container}>
         <View style={styles.topScreenName}>
           <Text>Feed</Text>
         </View>
 
-        <View>
+        <FlatList
+          refreshing={refresh}
+          onRefresh={this.loadNew}
+          data={photo_feed}
+          keyExtractor={(item, index) => index.toString()}
+          style={{ flex: 1, backgroundColor: "#eee" }}
+          renderItem={({ item, index }) => (
+            <View key={index}>
+              <View>
+                <Text>Time Ago</Text>
+                <Text>@Rusty</Text>
+              </View>
 
-          <View>
-            <Text>Time Ago</Text>
-            <Text>@Rusty</Text>
-          </View>
+              <View>
+                <Image
+                  source={{
+                    uri:
+                      "https://source.unsplash.com/random/500x" +
+                      Math.floor(Math.random() * 800 + 500)
+                  }}
+                  style={styles.imageStyles}
+                />
+              </View>
 
-          <View>
-            <Image
-              source={{
-                uri:
-                  "https://source.unsplash.com/random/500x" +
-                  Math.floor(Math.random() * 800 + 500)
-              }}
-              style={styles.imageStyles}
-            />
-          </View>
-
-          <View>
-            <Text>Caption text here...</Text>
-            <Text>View comments</Text>
-          </View>
-
-        </View>
+              <View>
+                <Text>Caption text here...</Text>
+                <Text>View comments</Text>
+              </View>
+            </View>
+          )}
+        />
       </View>
     );
   }
