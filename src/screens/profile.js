ab3a718631b35b89a5eca32c8309131269a8c30b
slavavasylencko <vyacheslav.vasylencko@itomy.ch>
2019-04-19 14:07:09 +0300

    Styled Profile screen

--- src/screens/profile.js
+++ src/screens/profile.js
@@ -3,9 +3,9 @@ import {
   StyleSheet,
   Text,
   View,
-  TextInput,
+  Image,
   Alert,
-  TouchableHighlight
+  TouchableOpacity
 } from "react-native";
 import { f, auth, database, storage } from "../../config/config.js";
 
@@ -31,13 +31,56 @@ class ProfileScreen extends Component {
     });
   }
   render() {
+    const { navigation } = this.props;
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View style={styles.container}>
         {this.state.loggedin === true ? (
           // are logged in
-          <Text>Profile</Text>
+          <View style={{ flex: 1 }}>
+            <View style={styles.topScreenName}>
+              <Text>Profile</Text>
+            </View>
+
+            <View style={styles.imageContainer}>
+              <Image
+                source={{
+                  uri: "https://api.adorable.io/avatars/285/test@user.i.png"
+                }}
+                style={styles.image}
+              />
+
+              <View style={{ marginRight: 10 }}>
+                <Text>Name</Text>
+                <Text>UserName</Text>
+              </View>
+            </View>
+
+            <View style={styles.buttonsBlock}>
+              <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Logout</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Edit Profile</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={[styles.button, { backgroundColor: "grey" }]}
+                onPress={() => navigation.navigate("Uploads")}
+              >
+                <Text style={[styles.buttonText, { color: "white" }]}>
+                  Upload New +
+                </Text>
+              </TouchableOpacity>
+            </View>
+
+            <View style={styles.loadingPhotos}>
+              <Text>Loading photos....</Text>
+            </View>
+
+          </View>
         ) : (
-          <View>
+          <View style={styles.notLoggedInContainer}>
             <Text>You are not logged in</Text>
             <Text> Please login to view your profile</Text>
           </View>
@@ -46,5 +89,59 @@ class ProfileScreen extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  topScreenName: {
+    height: 70,
+    paddingTop: 30,
+    backgroundColor: "white",
+    borderColor: "lightgrey",
+    borderBottomWidth: 0.5,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  notLoggedInContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  imageContainer: {
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    flexDirection: "row",
+    paddingVertical: 10
+  },
+  image: {
+    marginLeft: 10,
+    width: 100,
+    height: 100,
+    borderRadius: 50
+  },
+  buttonsBlock: {
+    paddingBottom: 20,
+    borderBottomWidth: 1
+  },
+  button: {
+    marginTop: 10,
+    marginHorizontal: 40,
+    paddingVertical: 15,
+    borderWidth: 1.5,
+    borderColor: "grey",
+    borderRadius: 20
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "grey"
+  },
+  loadingPhotos: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "green"
+  }
+});
 
 export default ProfileScreen;
