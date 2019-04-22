375ea6105fdc234d2b174a4f719275230129fb76
slavavasylencko <vyacheslav.vasylencko@itomy.ch>
2019-04-22 10:40:02 +0300

    Created function for generate unique id  for photo

--- src/screens/Upload.js
+++ src/screens/Upload.js
@@ -1,46 +1,95 @@
 import React, { Component } from "react";
-import {
-  Text,
-  View,
-} from "react-native";
-import {f} from "../../config/config";
+import { Text, View, StyleSheet, TouchableOpacity, Alert } from "react-native";
+import { f } from "../../config/config";
 
 class UploadScreen extends Component {
-    state = {
-        loggedin: false
-    };
-    componentDidMount() {
-        const that = this;
-
-        f.auth().onAuthStateChanged(function(user) {
-            if (user) {
-                //Logged in
-                that.setState({
-                    loggedin: true
-                });
-            } else {
-                // Not logged in
-                that.setState({
-                    loggedin: false
-                });
-            }
-        });
+    constructor(props){
+        super(props);
+        this.state = {
+            loggedin: false,
+            imageId: this.uniqueId()
+        };
+        Alert.alert(this.uniqueId())
     }
+
+
+  s4 = () => {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  };
+  uniqueId = () => {
+      console.log('s4', this.s4())
+    return (this.s4() + this.s4() + "-" + this.s4() + "-" + this.s4() + "-" +
+      this.s4() + "-" + this.s4() + "-" + this.s4() + "-" + this.s4()
+    );
+  };
+
+  findNewImage = () => {};
+
+  componentDidMount() {
+    const that = this;
+
+    f.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        //Logged in
+        that.setState({
+          loggedin: true
+        });
+      } else {
+        // Not logged in
+        that.setState({
+          loggedin: false
+        });
+      }
+    });
+  }
+
+
   render() {
     return (
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            {this.state.loggedin === true ? (
-                // are logged in
-                <Text>Upload</Text>
-            ) : (
-                <View>
-                    <Text>You are not logged in</Text>
-                    <Text> Please login to upload a photo</Text>
-                </View>
-            )}
-        </View>
+      <View style={styles.container}>
+        {this.state.loggedin === true ? (
+          // are logged in
+          <View style={styles.text}>
+            <Text style={styles.textUpload}>Upload</Text>
+            <TouchableOpacity
+              style={styles.buttonSelectPhoto}
+              onPress={() => this.findNewImage()}
+            >
+              <Text style={{ color: "white" }}>Select photo</Text>
+            </TouchableOpacity>
+          </View>
+        ) : (
+          <View style={styles.text}>
+            <Text>You are not logged in</Text>
+            <Text> Please login to upload a photo</Text>
+          </View>
+        )}
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  text: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  textUpload: {
+    fontSize: 28,
+    paddingBottom: 15
+  },
+  buttonSelectPhoto: {
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    backgroundColor: "blue",
+    borderRadius: 5
+  }
+});
 
 export default UploadScreen;
